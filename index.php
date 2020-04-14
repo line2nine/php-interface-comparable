0d32833e431c8dc380b4c0e825b8233efc216f44
line2nine <hailinh203@gmail.com>
2020-04-14 13:49:21 +0700

    Add files

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php
 include "class/ComparableCircle.php";
 
-$circleOne = new ComparableCircle('circleOne', 8);
-$circleTwo = new ComparableCircle('circleTwo', 2);
+$circles[0] = new ComparableCircle("Circle 1", 20);
+$circles[1] = new ComparableCircle("Circle 2", 17);
+$circles[2] = new ComparableCircle("Circle 3", 12);
 
-var_dump($circleOne->compareTo($circleTwo));
+echo('Pre-sort: <br>');
+foreach ($circles as $circle) {
+    echo('name: ' . $circle->getName() . ', radius: ' . $circle->getRadius() . '<br>');
+}
+sort($circles);
+echo('<br>After-sorted: <br>');
+foreach ($circles as $circle) {
+    echo('name: ' . $circle->getName() . ', radius: ' . $circle->getRadius() . '<br>');
+}
